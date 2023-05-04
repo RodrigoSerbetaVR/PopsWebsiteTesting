22d036bc5ac2eb4b7374bb3ca8e2e871505a7b23
RodrigoSerbetaVR <120190032+RodrigoSerbetaVR@users.noreply.github.com>
2023-05-04 21:57:33 +0300

    Delete LogOut.js

--- LogOut.js
+++
@@ -1,6 +0,0 @@
-const logoutBtn = document.getElementById("logoutBtn");
-
-logoutBtn.addEventListener("click", function() {
-  localStorage.removeItem("username"); // remove saved username
-  window.location.href = "LoggingOut.html"; // redirect to login page
-});
